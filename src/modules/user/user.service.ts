1905f650d52e669e6d1bff7d0c4ebf40fe864688
EvandroGiacomelli <evandrogiacomelli3@gmail.com>
2025-11-21 18:15:22 -0300

    feat(user): add user service
    Service com lógica de negócio de usuários.
    Separa lógica do resolver.

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/infrastructure/database/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  async findById(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+}
